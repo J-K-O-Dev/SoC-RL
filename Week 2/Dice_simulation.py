71a6fe5b7765986c36070d5b3d33ac7d9fce25fe
Jithin <jithinjithi226@gmail.com>
2026-06-13 14:47:56 +0530

    added the multi-armed-bandit

--- Week 2/Dice_simulation.py
+++ Week 2/Dice_simulation.py
@@ -24,21 +24,22 @@ num_rolls = 10
 results = roll_die(num_rolls)
 print(f"Results of rolling a die {num_rolls} times: {results}")
 
-def Theoritical_probability(face,rolls):
-    """
-    Calculates the theoretical probability of rolling a specific face on a six-sided die.
+# def Theoritical_probability(face,rolls):
+#     """
+#     Calculates the theoretical probability of rolling a specific face on a six-sided die.
 
-    Parameters:
-    face (int): The face for which to calculate the theoretical probability.
-    rolls (list): A list containing the results of each die roll.
+#     Parameters:
+#     face (int): The face for which to calculate the theoretical probability.
+#     rolls (list): A list containing the results of each die roll.
 
-    Returns:
-    float: The theoretical probability of rolling the specified face.
-    """
-    if 1 <= face <= 6:
-        return math.comb(len(rolls),rolls.count(face))*1/6**len(rolls)
-    else:
-        return 0
+#     Returns:
+#     float: The theoretical probability of rolling the specified face.
+#     """
+#     if 1 <= face <= 6:
+#         return math.comb(len(rolls),rolls.count(face))*1/6**len(rolls)*(5/6)**len(rolls)
+#     else:
+#         return 0
+# this was different from what we do in the calculate_probability_of_face so taking out 
 
 
 def calculate_probability_of_face(rolls, face):
@@ -66,18 +67,18 @@ for trial in range(10):
     rolls = roll_die(rolls_count)
     for face in range(1, 7):
        probability = {face: {"np.random probability":calculate_probability_of_face(rolls, face),
-                             "theoritical probability":Theoritical_probability(face,rolls)}} 
+                             "theoritical probability":1/6}} 
     print(f"trial {trial}: for {rolls_count} rolls probability of getting each face is {probability}")
 
 #RESULTS
-"""Results of rolling a die 10 times: [1, 2, 2, 5, 1, 1, 5, 4, 4, 2]
-trial 0: for 80 rolls probability of getting each face is {6: {'np.random probability': 0.0875, 'theoritical probability': 1.7777816305500147e-53}}
-trial 1: for 337 rolls probability of getting each face is {6: {'np.random probability': 0.18397626112759644, 'theoritical probability': 2.3866270059814968e-194}}
-trial 2: for 705 rolls probability of getting each face is {6: {'np.random probability': 0.16312056737588654, 'theoritical probability': 0.0}}
-trial 3: for 955 rolls probability of getting each face is {6: {'np.random probability': 0.1643979057591623, 'theoritical probability': 0.0}}
-trial 4: for 525 rolls probability of getting each face is {6: {'np.random probability': 0.1580952380952381, 'theoritical probability': 4.7032024391826e-311}}
-trial 5: for 503 rolls probability of getting each face is {6: {'np.random probability': 0.14512922465208747, 'theoritical probability': 5.853333522749436e-303}}
-trial 6: for 329 rolls probability of getting each face is {6: {'np.random probability': 0.16109422492401215, 'theoritical probability': 6.988321565792203e-195}}
-trial 7: for 211 rolls probability of getting each face is {6: {'np.random probability': 0.16113744075829384, 'theoritical probability': 1.394314516374227e-125}}
-trial 8: for 969 rolls probability of getting each face is {6: {'np.random probability': 0.17234262125902994, 'theoritical probability': 0.0}}
-trial 9: for 523 rolls probability of getting each face is {6: {'np.random probability': 0.17973231357552583, 'theoritical probability': 4.592518397620322e-302}}"""
+"""Results of rolling a die 10 times: [5, 3, 2, 3, 2, 4, 3, 4, 2, 6]
+trial 0: for 448 rolls probability of getting each face is {6: {'np.random probability': 0.16071428571428573, 'theoritical probability': 0.16666666666666666}}
+trial 1: for 424 rolls probability of getting each face is {6: {'np.random probability': 0.18160377358490565, 'theoritical probability': 0.16666666666666666}}
+trial 2: for 852 rolls probability of getting each face is {6: {'np.random probability': 0.1607981220657277, 'theoritical probability': 0.16666666666666666}}
+trial 3: for 85 rolls probability of getting each face is {6: {'np.random probability': 0.25882352941176473, 'theoritical probability': 0.16666666666666666}}
+trial 4: for 978 rolls probability of getting each face is {6: {'np.random probability': 0.18098159509202455, 'theoritical probability': 0.16666666666666666}}
+trial 5: for 710 rolls probability of getting each face is {6: {'np.random probability': 0.17746478873239438, 'theoritical probability': 0.16666666666666666}}
+trial 6: for 195 rolls probability of getting each face is {6: {'np.random probability': 0.17435897435897435, 'theoritical probability': 0.16666666666666666}}
+trial 7: for 556 rolls probability of getting each face is {6: {'np.random probability': 0.1672661870503597, 'theoritical probability': 0.16666666666666666}}
+trial 8: for 862 rolls probability of getting each face is {6: {'np.random probability': 0.17633410672853828, 'theoritical probability': 0.16666666666666666}}
+trial 9: for 894 rolls probability of getting each face is {6: {'np.random probability': 0.17114093959731544, 'theoritical probability': 0.16666666666666666}}"""
